a4d3ce249e673a03f9464b7f6eff2f407b57e2c3
PPartisan <PPartisan@users.noreply.github.com>
2016-05-31 14:02:36 +0100

    Adds Hex in addition to Binary.

--- app/src/main/java/com/werdpressed/partisan/texttobinary/MainActivity.java
+++ app/src/main/java/com/werdpressed/partisan/texttobinary/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -24,9 +25,16 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
 
     private static final String INPUT_TAG = "input_tag";
     private static final String OUTPUT_TAG = "output_tag";
+    private static final String STATE_TAG = "state_tag";
+
+    private static final int BINARY_OUTPUT = 6932;
+    private static final int HEX_OUTPUT = BINARY_OUTPUT + 1;
+
+    private int state = BINARY_OUTPUT;
 
     private EditText input;
     private TextView output;
+    private TextView title;
 
     private ClipboardManager clipboardManager;
 
@@ -37,6 +45,9 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
 
         input = (EditText) findViewById(R.id.input_et);
         output = (TextView) findViewById(R.id.output_tv);
+        title = (TextView) findViewById(R.id.binary_output_title_tv);
+
+        setOutputTitleString(R.string.binary);
 
         input.addTextChangedListener(this);
         output.setMovementMethod(new ScrollingMovementMethod());
@@ -53,9 +64,13 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
             output.setText(prefs.getString(OUTPUT_TAG, ERROR_STRING));
         }
 
+        state = prefs.getInt(STATE_TAG, BINARY_OUTPUT);
+
         if (savedInstanceState != null) {
             input.setText(savedInstanceState.getString(INPUT_TAG));
             output.setText(savedInstanceState.getString(OUTPUT_TAG));
+            state = savedInstanceState.getInt(STATE_TAG);
+            setOutputTitleString((state == HEX_OUTPUT) ? R.string.hex : R.string.binary);
         }
 
     }
@@ -65,14 +80,16 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
         super.onSaveInstanceState(outState);
         outState.putString(INPUT_TAG, input.getText().toString());
         outState.putString(OUTPUT_TAG, output.getText().toString());
+        outState.putInt(STATE_TAG, state);
     }
 
     @Override
-    protected void onDestroy() {
+    protected void onPause() {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefs.edit().putString(INPUT_TAG, input.getText().toString()).apply();
         prefs.edit().putString(OUTPUT_TAG, output.getText().toString()).apply();
-        super.onDestroy();
+        prefs.edit().putInt(STATE_TAG, state).apply();
+        super.onPause();
     }
 
     @Override
@@ -91,6 +108,16 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
                 return true;
             case R.id.action_share:
                 startActivity(getShareIntent(output.getText().toString()));
+                return true;
+            case R.id.action_swap_binary:
+                setOutputTitleString(R.string.binary);
+                state = BINARY_OUTPUT;
+                updateOutputText();
+                return true;
+            case R.id.action_swap_hex:
+                setOutputTitleString(R.string.hex);
+                state = HEX_OUTPUT;
+                updateOutputText();
                 return true;
         }
 
@@ -122,6 +149,41 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
 
     }
 
+    private static String getHexString(String input) {
+
+        StringBuilder builder = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            builder.append(Integer.toHexString(c));
+            builder.append(" ");
+        }
+
+        return builder.toString();
+    }
+
+    private void updateOutputText() {
+
+        output.setText(getOutputString(input.getText().toString()));
+
+    }
+
+    private String getOutputString(String input) {
+
+        switch (state) {
+            case BINARY_OUTPUT:
+                return getBinaryString(input);
+            case HEX_OUTPUT:
+                return getHexString(input);
+            default:
+                throw new AssertionError();
+        }
+
+    }
+
+    private void setOutputTitleString(int titleId) {
+        this.title.setText(getString(R.string.convert_title, getString(titleId)));
+    }
+
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -129,7 +191,7 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
 
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
-        output.setText(getBinaryString(s.toString()));
+        output.setText(getOutputString(s.toString()));
     }
 
     @Override
